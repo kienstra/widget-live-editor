4212dac42e0f3fddf90e5946f41d656779bf38cb
Ryan Kienstra <kienstraryan@gmail.com>
2014-11-24 16:50:22 -0600

    added tabs

--- js/wle-script.js
+++ js/wle-script.js
@@ -16,25 +16,25 @@
 
 		wp.customize( 'wle_options[image_slider_' + panel_name + ']' , function( value ) {
 			value.bind( function( to ) {
-	var max_height = 300; //should use wp_localize_script for this 
-	var percentage = to;
-	var img_height = Math.floor( max_height * percentage / 100 );
-	var img_height_in_pixels = String( img_height ) + 'px'; 
-	$( '.image_' + panel_name ).css( 'max-height' , img_height_in_pixels );
+				var max_height = 300; //should use wp_localize_script for this 
+				var percentage = to;
+				var img_height = Math.floor( max_height * percentage / 100 );
+				var img_height_in_pixels = String( img_height ) + 'px'; 
+				$( '.image_' + panel_name ).css( 'max-height' , img_height_in_pixels );
 			} );
 		} );
 
 		wp.customize( 'wle_options[image_' + panel_name + ']' , function( value ) {
 			value.bind( function( to ) {
-	$image = $( '.image_' + panel_name );
-	$image.attr( 'src', to );
-	var display = ( to ) ? 'block' : 'none';
-	$image.parent().css( 'display' , display );
+				$image = $( '.image_' + panel_name );
+				$image.attr( 'src', to );
+				var display = ( to ) ? 'block' : 'none';
+				$image.parent().css( 'display' , display );
 
-	is_svg = to.match( /.+\.svg$/ );
-	if ( is_svg ) {
-	 $image.css( 'height' , '300px' );
-	}
+				is_svg = to.match( /.+\.svg$/ );
+				if ( is_svg ) {
+					$image.css( 'height' , '300px' );
+				}
 			} );
 		} );
 	}
@@ -42,14 +42,14 @@
 	function heading_and_copy_bind( panel_name ) {
 		wp.customize( 'wle_options[heading_' + panel_name + ']' , function( value ) {
 			value.bind( function( to ) {
-	$( '.heading_' + panel_name ).html( to );
+				$( '.heading_' + panel_name ).html( to );
 			} );
 		} );
 
 		wp.customize( 'wle_options[copy_' + panel_name + ']' , function( value ) {
 			value.bind( function( to ) {
-	to = to.replace( /\n/g , '</br>' );
-	$( '.copy_' + panel_name ).html( to );
+				to = to.replace( /\n/g , '</br>' );
+				$( '.copy_' + panel_name ).html( to );
 			} );
 		} );
 	}
@@ -57,8 +57,8 @@
 	function link_href_bind( panel_name ) {
 		wp.customize( 'wle_options[link_href_' + panel_name + ']' , function( value ) {
 			value.bind( function( to ) {
-	var display = to ? 'inline-block' : 'none';
-	$( '#link_href_' + panel_name ).attr( 'href', to ).css( 'display' , display);
+				var display = to ? 'inline-block' : 'none';
+				$( '#link_href_' + panel_name ).attr( 'href', to ).css( 'display' , display);
 			} );
 		} );
 	}
