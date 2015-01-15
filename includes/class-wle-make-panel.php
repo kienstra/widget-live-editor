d7a0599cd56952aea37a09113bb91b6f48f7acbd
Ryan Kienstra <kienstraryan@gmail.com>
2015-01-14 22:19:01 -0400

    inline escaping

--- includes/class-wle-make-panel.php
+++ includes/class-wle-make-panel.php
@@ -4,11 +4,11 @@
 // Echoed in the widget() function of class WP_Widget_WLE
 
 class WLE_Make_Panel {
-	private static $instance;
-	private $panel_name;
-	private $container;
-	private $opening_div;
-	private $options;
+	protected static $instance;
+	protected $panel_name;
+	protected $container;
+	protected $opening_div;
+	protected $options;
 
 	public function __construct( $panel_name ) {
 		$this->panel_name = $panel_name;
@@ -39,19 +39,19 @@ class WLE_Make_Panel {
 		$height = $is_svg ? '300px' : '';
 		$max_height = $this->get_max_height();
 		$this->container .=
-			"<div class='wle-img-container' style='display: $display'>
-				<img class='img-customize {$selector} img-responsive' src='{$src}' style='max-height:{$max_height}; height: $height ' alt='{$alt}'>
-			</div> \n";
+			'<div class="wle-img-container" style="display: ' . esc_attr( $display ) . '">'
+			.	'<img class="img-customize ' . esc_attr( $selector ) . ' img-responsive" src="' . esc_url( $src ) . '" style="max-height:' . esc_attr( $max_height ) . '; height:'. esc_attr( $height ) . '" alt="' . esc_attr( $alt ) . '">'
+			. '</div>';
 	}
 
-	private function get_max_height() {
+	protected function get_max_height() {
 		$max_height_pixels = apply_filters( 'wle_image_max_height_in_pixels' , 300 );
 		$max_height_setting = $this->get_user_height_setting();
-		$max_height =	floor( $max_height_pixels * $max_height_setting / 100 );
+		$max_height = floor( $max_height_pixels * $max_height_setting / 100 );
 		return $max_height . 'px';
 	}
 
-	private function get_user_height_setting() {
+	protected function get_user_height_setting() {
 		$setting = isset ( $this->options[ 'image_slider_' . $this->panel_name ] ) ?
 					$this->options[ 'image_slider_' . $this->panel_name ] : 1;
 		return $setting;
@@ -62,26 +62,25 @@ class WLE_Make_Panel {
 		$heading_html = isset( $this->options[ $selector ] ) ?
 				       $this->options[ $selector ] : "";
 		$this->container .=
-			"<h2 class='{$selector} wle-heading'>
-			     {$heading_html}
-			 </h2>\n";
+			  '<h2 class="' . esc_attr( $selector ) . ' wle-heading">'
+			.	esc_html( $heading_html )
+			. '</h2>';
 	}
 
-	private function add_copy_and_link_section_to_container() {
-		$this->container .= "<div class='wle-copy-and-link'>";
+	protected function add_copy_and_link_section_to_container() {
+		$this->container .= '<div class="wle-copy-and-link">';
 		$this->add_copy_section_to_container();
 		$this->add_link_to_container();
-		$this->container .= "</div>\n";
+		$this->container .= '</div>';
 	}
 
 	public function add_copy_section_to_container() {
 		$selector = 'copy_' . $this->panel_name;
 		$copy_html = isset( $this->options[ $selector ] ) ? $this->options[ $selector ] : "";
-		$stripped_copy =	nl2br( strip_tags( $copy_html ) );
 		$this->container .=
-			"<span class='{$selector} wle-copy'>
-				 {$stripped_copy}
-			 </span>";
+			  '<span class="' . esc_attr( $selector ) . ' wle-copy">'
+			.	 nl2br( strip_tags( $copy_html ) )
+			. '</span>';
 	}
 
 	public function add_link_to_container() {
@@ -93,16 +92,20 @@ class WLE_Make_Panel {
 		$anchor_text = isset( $options[ 'anchor_text' ] ) ? $options[ 'anchor_text' ] : "";
 
 		$this->container .=
-			 "<p><a id='{$selector}' href='{$link_href}' class='{$anchor_class} wle-link' style='display: $display' >{$anchor_text}</a><p>\n";
+			  '<p>'
+			.	'<a id="' . esc_attr( $selector ) . '" href="' . esc_url( $link_href ) . '" class="' . esc_url( $anchor_class ) . ' wle-link" style="display: ' . esc_attr( $display ) . '">'
+			.		esc_html( $anchor_text )
+			.	'</a>'
+			. '<p>';
 	}
 
-	private function add_opening_div_to_container() {
+	protected function add_opening_div_to_container() {
 		$url_to_customizer_of_this_page = wle_get_url_to_customizer_of_this_page();
 		$this->container .=
-			"<div class='customized-col' id='{$this->panel_name}' >";
+			'<div class="customized-col" id="' . esc_attr( $this->panel_name ) . '">';
 	}
 
-	private function add_closing_div_to_container() {
+	protected function add_closing_div_to_container() {
 		$this->container .= '</div>';
 	}
 }	/* end WLE_Make_Panel */
